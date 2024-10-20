f3a0f5567bb7da993be9e45a40cb5ccaab601592
Laheem Ayub <tyou67906@gmail.com>
2024-10-20 06:18:40 +0500

    today paragraph part done

--- src/sections/home/GetStartedSec.js
+++ src/sections/home/GetStartedSec.js
@@ -1,12 +1,39 @@
 import React from 'react'
 import './GetStartedSec.css'
 import HoverImg from '../../components/HoverImg'
+import RightParaRow from '../../components/RightParaRow'
+import LeftParaRow from '../../components/LeftParaRow'
+import LeftRowData from '../../api/LeftRowData'
+import RightRowData from '../../api/RightRowData'
 
 const GetStartedSec = () => {
   return (
-    <div>
-      <HoverImg/>
-    </div>
+    <div className=''>
+      <HoverImg />
+
+        <div className='pt-4 para-contant'>
+          <LeftParaRow
+            title={LeftRowData[0].title}
+            para={LeftRowData[0].para}
+            img={LeftRowData[0].img}
+          />
+          <RightParaRow
+            title={RightRowData[0].title}
+            para={RightRowData[0].para}
+            img={RightRowData[0].img}
+          />
+          <LeftParaRow
+            title={LeftRowData[1].title}
+            para={LeftRowData[1].para}
+            img={LeftRowData[1].img}
+          />
+          <RightParaRow
+            title={RightRowData[1].title}
+            para={RightRowData[1].para}
+            img={RightRowData[1].img}
+          />
+        </div>
+      </div>
   )
 }
 
